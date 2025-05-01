2672f6cca3dbe86b7800c35dd9ca18b0c3ab0447
Ryu14n <ryushin0714@gmail.com>
2025-05-01 09:26:30 -0700

    replaced contact buttons with *ngFor

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  contacts = [
+    {
+      title: 'GitHub',
+      link: 'https://github.com/7-ry',
+      class: 'fab fa-github',
+    },
+    {
+      title: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/ryushin-oga-b61746344/',
+      class: 'fab fa-linkedin',
+    },
+    {
+      title: 'Email',
+      link: 'ryushin0714@gmail.com',
+      class: 'fas fa-envelope',
+    }
+  ]
 }
